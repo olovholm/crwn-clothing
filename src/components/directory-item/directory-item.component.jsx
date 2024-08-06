bfb811c7d39b9200e37ac62648586ece042a98be
Ola Lovholm <ola.lovholm@origo.oslo.kommune.no>
2024-08-06 12:57:50 +0200

    Refaktorerer directory, migrerer user.context til å bruke reducer framfor useState

--- src/components/directory-item/directory-item.component.jsx
+++ src/components/directory-item/directory-item.component.jsx
@@ -1,9 +1,13 @@
 import {BackgroundImage, Body, DirectoryItemContainer} from "./directory-item.styles";
+import {useNavigate} from "react-router-dom";
 
 const DirectoryItem = ({ category }) => {
-    const {imageUrl, title} = category
+    const {imageUrl, title, route} = category
+    const navigate = useNavigate()
+
+    const onNavigateHandler = () => navigate(route)
     return (
-        <DirectoryItemContainer>
+        <DirectoryItemContainer onClick={onNavigateHandler}>
             <BackgroundImage className="background-image"
                  imageUrl={imageUrl}
             />
